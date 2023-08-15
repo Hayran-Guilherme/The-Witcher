6520926b4321c5ce9fa2a6fe62ae5c72144630d9
Hayran Guilherme <hayran.gh@gmail.com>
2023-08-15 13:41:43 -0300

    Feat: First Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+      colors: {
+        backgroundButton : '#445454',
+        redEmphasis: '#BD0919',
+        textEmphasis: '#FFFFFF',
+        textDefault: '#D9D9D9',
+        gradient: 'linear-gradient(90deg, rgba(13,18,18,1) 70%, rgba(13,18,18,1) 70%, rgba(16,21,21,1) 100%)',
+      }
+    }
   },
   plugins: [],
 }
